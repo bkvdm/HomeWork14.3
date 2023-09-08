e2e0e1cd20242ca28d413e6c149fc44a74622df3
bkvdm <vadim@991199.ru>
2023-09-08 18:16:34 +0300

    Домашнее задание №14.3 //Напишите метод, который изменяет значение массива типа Integer. //Объявите метод changeValue, который принимает параметр массива типа Integer под названием value. //Внутри метода с помощью оператора присваивания запишите в параметр новый массив {1,2}. //В методе main объявите массив типа Integer под названием value и запишите туда значение {3,4}. //В следующей строке вызовите метод changeValue и передайте в него этот массив.

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void changeValue(Integer[] value) {
+        value = new Integer []{1, 2};
+
+        for (int i = 0; i < value.length; i++) {
+            System.out.println("value[i] = " + value[i]);
+        }
+    }
+
+    public static void main (String[] arg) {
+        Integer[] value = new Integer[]{3, 4};
+        changeValue(value);
+    }
+}
+
+//Напишите метод, который изменяет значение массива типа Integer.
+//Объявите метод changeValue, который принимает параметр массива типа Integer под названием value.
+//Внутри метода с помощью оператора присваивания запишите в параметр новый массив {1,2}.
+//В методе main объявите массив типа Integer под названием value и запишите туда значение {3,4}.
+//В следующей строке вызовите метод changeValue и передайте в него этот массив.
